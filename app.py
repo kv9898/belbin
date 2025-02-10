ac71f2b7ced8517fe98a890d8ffdd827dff0b42e
Dianyi Yang <105025148+kv9898@users.noreply.github.com>
2025-02-10 13:30:31 +0800

    Add result presentation

--- app.py
+++ app.py
@@ -94,6 +94,62 @@ def calculate_final_score() -> None:
         final_score = final_score_calculator(role, raw_score)
         role_scores[role] = final_score
 
+
+# Result table production
+file_path = "www/role_names.json"
+with open(file_path, encoding="utf-8") as file:
+    role_names: dict[str] = json.load(file)
+if role_names.keys() != role_scores.keys():
+    raise ValueError("Role names and scores do not match")
+
+results_df: pl.DataFrame = pl.DataFrame()
+styles = []  # for highlighting results
+
+
+def produce_results() -> None:
+    global results_df
+    global styles
+    results_df = pl.DataFrame(
+        {
+            "角色": [f"{role_names[role]} ({role})" for role in role_scores.keys()],
+            "分数": [role_scores[role] for role in role_scores.keys()],
+        }
+    )
+
+    def role_level(score: int) -> str:
+        if score >= 70:
+            return "自然角色"
+        elif score >= 30:
+            return "次要角色"
+        else:
+            return "避免角色"
+
+    results_df = results_df.with_columns(
+        pl.col("分数").map_elements(role_level, return_dtype=str).alias("角色等级")
+    )
+
+    # calculate styles
+    styles = []
+    natural_roles = [i for i, x in enumerate(results_df["角色等级"] == "自然角色") if x]
+    if len(natural_roles) > 0:
+        styles.append(
+            {
+                "rows": natural_roles,
+                "style": {"font-weight": "bold", "background-color": "#bfffaf"},
+            }
+        )
+    avoided_roles = [i for i, x in enumerate(results_df["角色等级"] == "避免角色") if x]
+    if len(avoided_roles) > 0:
+        styles.append(
+            {
+                "rows": avoided_roles,
+                "style": {"background-color": "#ffbaaf"},
+            }
+        )
+
+produce_results()
+
+
 # Check for last choice of the last question
 def last_choice(question: int, choice: str) -> bool:
     try:
@@ -180,17 +236,36 @@ for question in questionnaire:
         raise ValueError("Invalid question found")
     question_panels.append(get_question_panel(int(match)))
 
+# create results panel
+results_panel = ui.nav_panel(
+    "结果",
+    ui.strong("您的贝尔宾团队角色自我认知问卷最终结果："),
+    ui.br(),
+    ui.br(),
+    ui.output_data_frame("results"),
+)
+
+# compile ui
 app_ui = ui.page_fluid(
     ui.head_content(ui.include_js("app_py.js")),
-    ui.navset_tab(
-        welcome_panel,
-        *question_panels,
-        id="main_tab"
+    ui.navset_hidden(
+        ui.nav_panel(
+            "main_tab",
+            ui.navset_tab(
+                    welcome_panel, 
+                    *question_panels, 
+                    id="main_tab"
+            )
+        ),
+    results_panel,
+    id="results_display"
     )
 )
 
 def server(input, output, session):
-    
+
+    results_df_reac =  reactive.value(results_df) # for reactive results handling
+
     def next_tab(question: Optional[int] = None, choice: Optional[str] = None):
         next_q: int = 1
         next_c: str = "A"
@@ -243,6 +318,10 @@ def server(input, output, session):
             role_score_calculate()
             calculate_final_score()
             print(json.dumps(role_scores, indent=2))
+            produce_results()
+            results_df_reac.set(results_df)
+            ui.update_navs(id="results_display", selected="结果")
+
         return button_processor
     
     for question in questionnaire:
@@ -265,5 +344,13 @@ def server(input, output, session):
                     continue
                 slider_input = getattr(input, f"q{q}{c}")
                 answers[f"{q}"][c] = slider_input()
+
+    # display results
+    @render.data_frame
+    def results():
+        return render.DataGrid(
+            results_df_reac(), styles=styles
+        )
+
 
 app = App(app_ui, server, static_assets=Path(__file__).parent / "www", debug=True)
