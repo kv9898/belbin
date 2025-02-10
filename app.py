94b50963a0aa6bf99d60ba829fc1975679df0961
Dianyi Yang <105025148+kv9898@users.noreply.github.com>
2025-02-10 10:32:30 +0800

    main: add final score calculation

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from pathlib import Path
 from typing import Optional
 from collections.abc import Callable
 from string import ascii_letters
-import json, re
+import csv, json, re
+import polars as pl
 
 # Read questionnaire
 # file_path = Path(__file__).parent / "www" / "questionnaire.json"
@@ -41,6 +42,7 @@ with open(file_path, encoding="utf-8") as file:
     role_scores_table: dict[str] = json.load(file)
 role_scores: dict[float] = {role: 0 for role in role_scores_table.keys()}
 
+# Role score calculation
 def role_score_calculate() -> None:
     for role in role_scores:
         score = 0
@@ -48,6 +50,49 @@ def role_score_calculate() -> None:
             choice = role_scores_table[role][str(q)].lower()
             score += answers[str(q)][choice]
         role_scores[role] = score
+
+# Final score calculation
+final_score_table: pl.DataFrame = pl.read_csv("www/final.csv", separator=",")  
+
+def final_score_calculator(role: str, raw_score: float) -> int:
+    if role not in final_score_table.columns:
+        raise ValueError(f"Role {role} not found in final score table")
+    
+    role_data = final_score_table[["raw", role]]
+    
+    # return if raw score is in the table
+    if raw_score in role_data["raw"]:
+        processed_score: pl.Series = role_data.filter(pl.col("raw") == raw_score).get_column(role)
+        if len(processed_score) != 1:
+            raise ValueError(f"Multiple final scores found for raw score {raw_score} and role {role}")
+        processed_score: int = processed_score[0]
+        if not isinstance(processed_score,  int) or processed_score < 0:
+            raise ValueError(f"Invalid final score {processed_score} found for raw score {raw_score} and role {role}")
+        return processed_score
+    
+    # deal with out of range raw score
+    if raw_score >= role_data[0,0]:
+        return role_data[0,1]
+    elif raw_score <= role_data[-1,0]:
+        return role_data[-1,1]
+    
+    # find interval for decimal raw score
+    for i in range(role_data.height - 1):
+        x_high, y_high = role_data[i].rows()[0]
+        x_low, y_low = role_data[i+1].rows()[0]
+        if x_high >= raw_score >= x_low:
+            break
+    
+    # 计算插值
+    slope = (y_low - y_high) / (x_low - x_high)
+    processed_score = y_high + slope * (raw_score - x_high)
+    return int(processed_score)
+
+def calculate_final_score() -> None:
+    for role in role_scores:
+        raw_score = role_scores[role]
+        final_score = final_score_calculator(role, raw_score)
+        role_scores[role] = final_score
 
 # Check for last choice of the last question
 def last_choice(question: int, choice: str) -> bool:
@@ -195,8 +240,8 @@ def server(input, output, session):
         def button_processor():
             collect_answers()
             normalise_answers()
-            print(json.dumps(answers, indent=2))
             role_score_calculate()
+            calculate_final_score()
             print(json.dumps(role_scores, indent=2))
         return button_processor
     
