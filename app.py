98cbb7edccc382f6b792e6bb976f9e673f74c1b3
Dianyi Yang <105025148+kv9898@users.noreply.github.com>
2025-02-10 13:30:35 +0800

    formatting

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from typing import Optional
 from collections.abc import Callable
 from string import ascii_letters
-import csv, json, re
+import json, re
 import polars as pl
 
 # Read questionnaire
@@ -23,9 +23,10 @@ for index, question in enumerate(questionnaire):
         raise ValueError("Question text not found")
     else:
         UPPERCASES = list(ascii_letters[26:])
-        if choices != UPPERCASES[:len(choices)]:
+        if choices != UPPERCASES[: len(choices)]:
             raise ValueError("Choices key mismatch")
     answers[f"{index + 1}"] = {choice.lower(): 0 for choice in choices}
+
 
 # normalise answers to sum to 100
 def normalise_answers() -> None:
@@ -36,11 +37,13 @@ def normalise_answers() -> None:
         for choice in answers[question]:
             answers[question][choice] = answers[question][choice] / total * 10
 
+
 # Role score initialisation
 file_path = "www/roles.json"
 with open(file_path, encoding="utf-8") as file:
     role_scores_table: dict[str] = json.load(file)
 role_scores: dict[float] = {role: 0 for role in role_scores_table.keys()}
+
 
 # Role score calculation
 def role_score_calculate() -> None:
@@ -51,42 +54,51 @@ def role_score_calculate() -> None:
             score += answers[str(q)][choice]
         role_scores[role] = score
 
+
 # Final score calculation
-final_score_table: pl.DataFrame = pl.read_csv("www/final.csv", separator=",")  
+final_score_table: pl.DataFrame = pl.read_csv("www/final.csv", separator=",")
+
 
 def final_score_calculator(role: str, raw_score: float) -> int:
     if role not in final_score_table.columns:
         raise ValueError(f"Role {role} not found in final score table")
-    
+
     role_data = final_score_table[["raw", role]]
-    
+
     # return if raw score is in the table
     if raw_score in role_data["raw"]:
-        processed_score: pl.Series = role_data.filter(pl.col("raw") == raw_score).get_column(role)
+        processed_score: pl.Series = role_data.filter(
+            pl.col("raw") == raw_score
+        ).get_column(role)
         if len(processed_score) != 1:
-            raise ValueError(f"Multiple final scores found for raw score {raw_score} and role {role}")
+            raise ValueError(
+                f"Multiple final scores found for raw score {raw_score} and role {role}"
+            )
         processed_score: int = processed_score[0]
-        if not isinstance(processed_score,  int) or processed_score < 0:
-            raise ValueError(f"Invalid final score {processed_score} found for raw score {raw_score} and role {role}")
+        if not isinstance(processed_score, int) or processed_score < 0:
+            raise ValueError(
+                f"Invalid final score {processed_score} found for raw score {raw_score} and role {role}"
+            )
         return processed_score
-    
+
     # deal with out of range raw score
-    if raw_score >= role_data[0,0]:
-        return role_data[0,1]
-    elif raw_score <= role_data[-1,0]:
-        return role_data[-1,1]
-    
+    if raw_score >= role_data[0, 0]:
+        return role_data[0, 1]
+    elif raw_score <= role_data[-1, 0]:
+        return role_data[-1, 1]
+
     # find interval for decimal raw score
     for i in range(role_data.height - 1):
         x_high, y_high = role_data[i].rows()[0]
-        x_low, y_low = role_data[i+1].rows()[0]
+        x_low, y_low = role_data[i + 1].rows()[0]
         if x_high >= raw_score >= x_low:
             break
-    
+
     # 计算插值
     slope = (y_low - y_high) / (x_low - x_high)
     processed_score = y_high + slope * (raw_score - x_high)
     return int(processed_score)
+
 
 def calculate_final_score() -> None:
     for role in role_scores:
@@ -163,9 +175,10 @@ def last_choice(question: int, choice: str) -> bool:
     except (ValueError, IndexError):
         return False
 
+
 # Produce welcome panel
 welcome_panel = ui.nav_panel(
-    "欢迎", 
+    "欢迎",
     ui.strong("贝尔宾团队角色自我认知问卷"),
     ui.br(),
     ui.br(),
@@ -174,8 +187,9 @@ welcome_panel = ui.nav_panel(
     """,
     ui.br(),
     ui.br(),
-    ui.input_action_button("start_button","开始问卷")
-    )
+    ui.input_action_button("start_button", "开始问卷"),
+)
+
 
 # Produce choice panel
 def get_choice_panel(question: int, choice: str) -> NavPanel:
@@ -195,9 +209,10 @@ def get_choice_panel(question: int, choice: str) -> NavPanel:
         choice.upper(),
         choice_txt,
         ui.input_slider(f"q{question}{choice}", "", 0, 10, 0),
-        ui.input_action_button(f"next{question}{choice}", button_text)
+        ui.input_action_button(f"next{question}{choice}", button_text),
     )
     return choice_panel
+
 
 # Produce question panel
 def get_question_panel(question: int) -> NavPanel:
@@ -218,20 +233,16 @@ def get_question_panel(question: int) -> NavPanel:
             continue
         choice_panels.append(get_choice_panel(question, choice))
     question_panel = ui.nav_panel(
-        str(question),
-        question_txt,
-        ui.navset_tab(
-        *choice_panels,
-        id=f"q{question}"
-        )
+        str(question), question_txt, ui.navset_tab(*choice_panels, id=f"q{question}")
     )
     return question_panel
+
 
 # get question panels
 question_panels: list[NavPanel] = []
 for question in questionnaire:
     try:
-        match = int(re.match(r'^Q(\d+)$', question).group(1))
+        match = int(re.match(r"^Q(\d+)$", question).group(1))
     except:
         raise ValueError("Invalid question found")
     question_panels.append(get_question_panel(int(match)))
@@ -261,6 +272,7 @@ app_ui = ui.page_fluid(
     id="results_display"
     )
 )
+
 
 def server(input, output, session):
 
@@ -300,13 +312,15 @@ def server(input, output, session):
     def create_button_processor(question: int, choice: str) -> Callable:
         choice = choice.lower()
         input_event = getattr(input, f"next{question}{choice}")
+
         @reactive.effect
         @reactive.event(input_event)
         def button_processor():
             next_tab(question, choice)
             collect_answers()
+
         return button_processor
-    
+
     def submit_button_processor(question: int, choice: str) -> Callable:
         choice = choice.lower()
         input_event = getattr(input, f"next{question}{choice}")
@@ -323,22 +337,22 @@ def server(input, output, session):
             ui.update_navs(id="results_display", selected="结果")
 
         return button_processor
-    
+
     for question in questionnaire:
         for choice in questionnaire[question]:
-            q = int(re.match(r'^Q(\d+)$', question).group(1))
+            q = int(re.match(r"^Q(\d+)$", question).group(1))
             c = choice.lower()
             if choice == "q":
                 continue
             if last_choice(q, c):
                 globals()[f"button{q}{c}"] = submit_button_processor(q, c)
             globals()[f"button{q}{c}"] = create_button_processor(q, c)
-    
+
     # collect slider answers
     def collect_answers():
         for question in questionnaire:
             for choice in questionnaire[question]:
-                q = int(re.match(r'^Q(\d+)$', question).group(1))
+                q = int(re.match(r"^Q(\d+)$", question).group(1))
                 c = choice.lower()
                 if choice == "q":
                     continue
